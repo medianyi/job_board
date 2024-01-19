fcc8f2316aa37a635ad9139a8e7abf8a61717c6a
Medianyi <vladimirmedianyi@gmail.com>
2024-01-19 18:04:16 +0300

    add templates\job_board\index.html

--- job_board/models.py
+++ job_board/models.py
@@ -9,9 +9,16 @@ from django.db import models
 ## title, description, company, salary
 
 class JobPosting(models.Model):
-    title = models.CharField(max_lenght=100)
+    # id - starts at 1 and autoincrements
+    title = models.CharField(max_length=100)
     description = models.TextField()
-    company = models.CharField(max_lenght=100)
+    company = models.CharField(max_length=100)
     salary = models.IntegerField()
+    is_active = models.BooleanField(default=False)
 
 
+# CRUD
+# create
+# read
+# update
+# delete
--- job_board/views.py
+++ job_board/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import JobPosting
+
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Job Board</h1>')
+    active_postings = JobPosting.objects.filter(is_active=True)
+    context = {
+        'job_posting': active_postings
+    }
+    return render(request, 'job_board/index.html', context)
