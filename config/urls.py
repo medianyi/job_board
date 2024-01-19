23034494334a33b3e627fb4a3b57edd633c7b8fc
Medianyi <vladimirmedianyi@gmail.com>
2024-01-19 15:56:00 +0300

    fix job_board/urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(git init'job_board.urls')),
+    path('', include('job_board.urls')),
 ]
