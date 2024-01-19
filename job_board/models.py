b960c4901b2f4e6b22c706d5b46067fa57470d53
Medianyi <vladimirmedianyi@gmail.com>
2024-01-19 16:01:46 +0300

    add class JobPosting

--- job_board/models.py
+++ job_board/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# model => python class
+# model represents a table in the db
+# attrs are the fields in the table
+
+# Job posting tablet
+## title, description, company, salary
+
+class JobPosting(models.Model):
+    title = models.CharField(max_lenght=100)
+    description = models.TextField()
+    company = models.CharField(max_lenght=100)
+    salary = models.IntegerField()
+
+
