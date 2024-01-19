d4966dbebebf7b09c9d9ad6c7fa45e46e6f22539
Medianyi <vladimirmedianyi@gmail.com>
2024-01-19 18:42:08 +0300

    add def __str__ in models

--- job_board/admin.py
+++ job_board/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from job_board.models import JobPosting
+
 # Register your models here.
+
+
+admin.site.register(JobPosting)
--- job_board/models.py
+++ job_board/models.py
@@ -16,6 +16,9 @@ class JobPosting(models.Model):
     salary = models.IntegerField()
     is_active = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"{self.title} | {self.company} | Active: {self.is_active}"
+
 
 # CRUD
 # create
